0fddd78b116b50da7df42d5f3cdb67cd858d9419
Simon Vieille <simon@deblan.fr>
2021-05-16 15:26:27 +0200

    add collection block

---
+++ core/Entity/Site/Page/CollectionBlock.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Core\Entity\Site\Page;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+
+/**
+ * @ORM\Entity
+ */
+class CollectionBlock extends Block
+{
+    public function getValue()
+    {
+        return json_decode(parent::getValue(), true);
+    }
+
+    public function setValue($value): self
+    {
+        return parent::setValue(json_encode($value));
+    }
+}
---
+++ core/Form/Site/Page/CollectionBlockType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Core\Form\Site\Page;
+
+use App\Core\Entity\Site\Page\Block;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\CallbackTransformer;
+
+class CollectionBlockType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'value',
+            CollectionType::class,
+            array_merge([
+                'required' => false,
+                'label' => false,
+            ], $options['options']),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_replace($view->vars, [
+            'collection_name' => $options['collection_name'],
+            'label_add' => $options['label_add'],
+            'label_delete' => $options['label_delete'],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Block::class,
+            'collection_name' => '',
+            'label_add' => 'Add',
+            'label_delete' => 'Delete',
+            'options' => [],
+        ]);
+    }
+}
