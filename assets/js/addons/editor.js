62d1f09f6e98454c2e127b9c7a9a5e723ef8ef6c
Simon Vieille <simon@deblan.fr>
2021-05-16 13:20:33 +0200

    add tinymce plugin link

--- assets/js/addons/editor.js
+++ assets/js/addons/editor.js
@@ -8,7 +8,7 @@ module.exports = function() {
       base_url: '/vendor/tinymce/',
       cache_suffix: '?v=4.1.6',
       language: 'fr_FR',
-      plugins: 'print preview importcss searchreplace visualblocks visualchars fullscreen template table charmap hr pagebreak nonbreaking toc insertdatetime advlist lists wordcount textpattern noneditable help charmap quickbars',
+      plugins: 'print preview importcss searchreplace visualblocks visualchars fullscreen template table charmap hr pagebreak nonbreaking toc insertdatetime advlist lists wordcount textpattern noneditable help charmap quickbars link',
       menubar: 'file edit view insert format tools table tc help',
       toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap | fullscreen  preview | code',
       importcss_append: true,
