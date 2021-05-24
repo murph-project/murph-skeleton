dbeb90dca8e724e784fe7e0f387be6f69673e81d
Simon Vieille <simon@deblan.fr>
2021-05-24 20:45:05 +0200

    fix modal loading

--- assets/js/addons/modal.js
+++ assets/js/addons/modal.js
@@ -14,14 +14,14 @@ module.exports = function() {
             body.append(container);
         }
 
-        const loader = $('<div style="position: absolute; top: 25vh; left: 50vw; z-index: 2000">')
-        loader.html('<div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>')
-        body.append(loader)
+        const loader = $('<div style="position: absolute; top: 25vh; left: 50vw; z-index: 2000">');
+        loader.html('<div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>');
+        body.append(loader);
 
-        container.html();
+        container.html('');
 
         const url = $(e.target).attr('data-modal');
-            $(container).modal('show');
+        $(container).modal('show');
 
         container.load(url, function() {
             loader.remove()
