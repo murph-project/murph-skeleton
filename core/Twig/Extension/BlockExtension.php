34ac2280c85829350ff2482f8573e2f47eb333b0
Simon Vieille <simon@deblan.fr>
2021-05-29 12:52:03 +0200

    add insertable tag to generate link to node inside content

---
+++ core/Twig/Extension/BlockExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Core\Twig\Extension;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class BlockExtension extends AbstractExtension
+{
+    protected UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('murph_url', [$this, 'replaceUrl']),
+        ];
+    }
+
+    public function replaceUrl($content)
+    {
+        preg_match_all('#\{\{\s*url://(?P<route>[a-z_]+)(\?(?P<params>.*))?\s*\}\}#isU', $content, $match, PREG_SET_ORDER);
+
+        foreach ($match as $block) {
+            $url = null;
+
+            try {
+                $block['params'] = str_replace(['&amp;', ' '], ['&', '%20'], $block['params']);
+                $route = $block['route'];
+                parse_str($block['params'], $params);
+
+                $url = $this->urlGenerator->generate($route, $params, UrlGeneratorInterface::ABSOLUTE_URL);
+            } catch (\Exception $e) {
+            }
+
+            $content = str_replace($match[0], $url, $content);
+        }
+
+        return $content;
+    }
+}
