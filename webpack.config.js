d7659735cb53d846b01ec8e57fb6982b69655b56
Simon Vieille <simon@deblan.fr>
2022-02-21 23:58:08 +0100

    update default template

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('admin', './assets/js/admin.js')
+    .addEntry('app', './assets/js/app.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
