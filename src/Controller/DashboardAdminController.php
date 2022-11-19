58de4e8908729a8f55b99882a8e2a515af30ccc7
Simon Vieille <simon@deblan.fr>
2022-11-19 19:46:03 +0100

    replace annotation with attributes

--- src/Controller/DashboardAdminController.php
+++ src/Controller/DashboardAdminController.php
@@ -6,14 +6,10 @@ use App\Core\Controller\Dashboard\DashboardAdminController as Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/admin")
- */
+#[Route(path: '/admin')]
 class DashboardAdminController extends Controller
 {
-    /**
-     * @Route("/", name="admin_dashboard_index")
-     */
+    #[Route(path: '/', name: 'admin_dashboard_index')]
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig');
